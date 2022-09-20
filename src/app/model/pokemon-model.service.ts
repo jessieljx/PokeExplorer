a0a94b89e0ce83892ca37807274b34aca470e424
John Brink <john-brink@hotmail.com>
2022-09-20 15:00:19 +0200

    fetch done

--- src/app/model/pokemon-model.service.ts
+++ src/app/model/pokemon-model.service.ts
@@ -1,21 +1,33 @@
 import { VariableBinding } from '@angular/compiler';
 import { Inject } from '@angular/core';
 
+
 @Inject({
   providedIn: 'root'
 })
 export class PokemonModelService {
   pokemonId: number;
   promiseResult: Promise<any>;
+  data : any
 
   constructor(id: number = 1) {
     this.pokemonId = id;
-    this.promiseResult = fetch("https://pokeapi.co/api/v2/pokemon/" + this.pokemonId).then(r=>r.json());
-  }
+    this.promiseResult = this.fetchAPI();
+    this.data;
+    
+    
+}
+
   setPokemonId(id: number){
     if(id===this.pokemonId)
       return;
     this.pokemonId=id;
+    this.promiseResult = this.fetchAPI();
+  }
+
+  fetchAPI() {
+    return fetch("https://pokeapi.co/api/v2/pokemon/" + this.pokemonId).then(response=> this.data = response.json()).then(()=>console.log(this.data)).catch(error => console.log(error))
+  
   }
 }
 
