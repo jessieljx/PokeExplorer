36aace094dedf2188658cff5dbfb67e5b5cdd0b4
Erik Meurk <60180870+erikaxelmeurk@users.noreply.github.com>
2022-09-20 11:50:54 +0200

    Numbereditor and Poke-component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './views/pokemon/pokemon.component';
+import { NumberEditorComponent } from './views/number-editor/number-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent,
+    NumberEditorComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/number-editor/number-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-editor',
+  templateUrl: './number-editor.component.html',
+  styleUrls: ['./number-editor.component.css']
+})
+export class NumberEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
